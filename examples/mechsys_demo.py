e362c61e72ec146b085e8f32829a91d8bcdaab66
Leon Seidel <leon.seidel@fau.de>
2024-11-23 22:36:05 +0100

    Update examples

--- examples/mechsys_demo.py
+++ examples/mechsys_demo.py
@@ -1,18 +1,24 @@
+# Fix needed
 import asyncio
 import time
-from mechsys_uav import UAV
+from mechsys_uav.mechsys_uav import UAV
 
 async def main():
     uav = await UAV.connect(use_sim=True)
-    print(uav.get_position)
+    pos = uav.get_position()
+    print(pos)
     
     # Takeoff
     goal_accepted = await uav.arm_and_takeoff(takeoff_altitude=2)
 
-    while uav.get_position[2] < 1.9:
-        print(uav.get_position[2])
-        print("Waiting for takeoff altitude")
-        time.sleep(1)
+    while True:
+        pos = uav.get_position()
+        if pos[2] < 1.9:
+            print(pos[2])
+            print("Waiting for takeoff altitude")
+            time.sleep(1)
+        else:
+            break
     
     # Fly towards goal for 5 seconds
     goal_accepted = await uav.send_goal_position(latitude=37.413240, longitude=-121.999524, relative_altitude=8, heading=11)
